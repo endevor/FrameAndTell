5b3b22c2d6d70298a67b306b3d1a64da51a60bd4
runigoswami <imac@macintosh-1357.wireless.dartmouth.edu>
2014-05-20 14:39:29 -0400

    UI Elements
    First commit of the new UI elements, please don’t make any changes to
    the xml!

--- FrameAndTell/gen/edu/dartmouth/cs/frameandtell/R.java
+++ FrameAndTell/gen/edu/dartmouth/cs/frameandtell/R.java
@@ -21,8 +21,55 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int blankbox=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int ab_background_textured_frameandtell=0x7f020000;
+        public static final int ab_bottom_solid_frameandtell=0x7f020001;
+        public static final int ab_solid_frameandtell=0x7f020002;
+        public static final int ab_stacked_solid_frameandtell=0x7f020003;
+        public static final int ab_texture_tile_frameandtell=0x7f020004;
+        public static final int ab_transparent_frameandtell=0x7f020005;
+        public static final int add_image_btn=0x7f020006;
+        public static final int add_text_btn=0x7f020007;
+        public static final int blankbox=0x7f020008;
+        public static final int book_icon=0x7f020009;
+        public static final int btn_cab_done_default_frameandtell=0x7f02000a;
+        public static final int btn_cab_done_focused_frameandtell=0x7f02000b;
+        public static final int btn_cab_done_frameandtell=0x7f02000c;
+        public static final int btn_cab_done_pressed_frameandtell=0x7f02000d;
+        public static final int cab_background_bottom_frameandtell=0x7f02000e;
+        public static final int cab_background_top_frameandtell=0x7f02000f;
+        public static final int delete_btn=0x7f020010;
+        public static final int dialog_cancel_btn=0x7f020011;
+        public static final int dialog_save_btn=0x7f020012;
+        public static final int down_arrow_icon=0x7f020013;
+        public static final int home_bkgd_img=0x7f020014;
+        public static final int home_icon=0x7f020015;
+        public static final int ic_launcher=0x7f020016;
+        public static final int list_focused_frameandtell=0x7f020017;
+        public static final int list_pressed_frameandtell=0x7f020018;
+        public static final int menu_dropdown_panel_frameandtell=0x7f020019;
+        public static final int plus_btn=0x7f02001a;
+        public static final int progress_bg_frameandtell=0x7f02001b;
+        public static final int progress_horizontal_frameandtell=0x7f02001c;
+        public static final int progress_primary_frameandtell=0x7f02001d;
+        public static final int progress_secondary_frameandtell=0x7f02001e;
+        public static final int save_btn=0x7f02001f;
+        public static final int selectable_background_frameandtell=0x7f020020;
+        public static final int side_arrow_btn=0x7f020021;
+        public static final int spinner_ab_default_frameandtell=0x7f020022;
+        public static final int spinner_ab_disabled_frameandtell=0x7f020023;
+        public static final int spinner_ab_focused_frameandtell=0x7f020024;
+        public static final int spinner_ab_pressed_frameandtell=0x7f020025;
+        public static final int spinner_background_ab_frameandtell=0x7f020026;
+        public static final int tab_indicator_ab_frameandtell=0x7f020027;
+        public static final int tab_selected_focused_frameandtell=0x7f020028;
+        public static final int tab_selected_frameandtell=0x7f020029;
+        public static final int tab_selected_pressed_frameandtell=0x7f02002a;
+        public static final int tab_unselected_focused_frameandtell=0x7f02002b;
+        public static final int tab_unselected_frameandtell=0x7f02002c;
+        public static final int tab_unselected_pressed_frameandtell=0x7f02002d;
+        public static final int timeline_icon=0x7f02002e;
+        public static final int undo_btn=0x7f02002f;
+        public static final int yellow_background=0x7f020030;
     }
     public static final class id {
         public static final int action_settings=0x7f080005;
@@ -41,7 +88,7 @@ public final class R {
     }
     public static final class menu {
         public static final int edit_panel=0x7f070000;
-        public static final int home=0x7f070001;
+        public static final int home_actions=0x7f070001;
         public static final int new_story=0x7f070002;
         public static final int reader=0x7f070003;
         public static final int timeline=0x7f070004;
@@ -90,6 +137,10 @@ public final class R {
         public static final int todo=0x7f050003;
     }
     public static final class style {
+        public static final int ActionBar_Solid_Frameandtell=0x7f060003;
+        public static final int ActionBar_Transparent_Frameandtell=0x7f060005;
+        public static final int ActionBarTabStyle_Frameandtell=0x7f060008;
+        public static final int ActionButton_CloseMode_Frameandtell=0x7f06000b;
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
@@ -116,5 +167,14 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f060001;
+        public static final int DropDownListView_Frameandtell=0x7f060007;
+        public static final int DropDownNav_Frameandtell=0x7f060009;
+        public static final int NoTitleText=0x7f060004;
+        public static final int PopupMenu_Frameandtell=0x7f060006;
+        public static final int ProgressBar_Frameandtell=0x7f06000a;
+        public static final int Theme_Frameandtell=0x7f060002;
+        /**  this style is only referenced in a Light.DarkActionBar based theme 
+         */
+        public static final int Theme_Frameandtell_Widget=0x7f06000c;
     }
 }
--- FrameAndTell/src/edu/dartmouth/cs/frameandtell/HomeActivity.java
+++ FrameAndTell/src/edu/dartmouth/cs/frameandtell/HomeActivity.java
@@ -17,7 +17,7 @@ public class HomeActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.home, menu);
+		getMenuInflater().inflate(R.menu.home_actions, menu);
 		return true;
 	}
 
